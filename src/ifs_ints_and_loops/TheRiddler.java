55fbef8d74dd1842407d36d0b11cfd484f8a29c5
pigchick3n <League@administorsimac.attlocal.net>
2018-08-18 14:27:31 -0700

    Module 1

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -12,17 +12,28 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
- int score=0;
+		int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-  JOptionPane.showInputDialog("");
+		String answer=  JOptionPane.showInputDialog("Which word in the dictionary is spelled incorrectly?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(answer.equals ("incorrectly")) { score++;
+		JOptionPane.showMessageDialog(null, "correct!");
 
 		// 5. Otherwise, say "wrong" and tell them the answer
+		}else {	JOptionPane.showMessageDialog(null, "Wrong!");
+		JOptionPane.showMessageDialog(null, "Correct answer is: incorrectly");}
+
 
 		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
 		
+		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "score "+ score);
 	}
 }
+
+
+
+
+
+
 
